763c12743ac9e67945391a534ffd8bac28c3dc36
Kitty Fung <magd5245@htc-login02.arcus-htc.arc.local>
2023-01-15 09:25:11 +0000

    first my babe

---
+++ Work/ipd_environment.py
@@ -0,0 +1,144 @@
+import torch
+import os.path as osp
+import gym
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# def one_iteration_value(self, payout_mat_1, payout_mat_2):
+#     dims = [1, 1]
+#     def Ls(th):
+#         p_1, p_2 = torch.sigmoid(th[0]), torch.sigmoid(th[1])
+#         x, y = torch.cat([p_1, 1-p_1], dim=-1), torch.cat([p_2, 1-p_2], dim=-1)
+#         L_1 = -torch.matmul(torch.matmul(x.unsqueeze(1), payout_mat_1), y.unsqueeze(-1))
+#         L_2 = -torch.matmul(torch.matmul(x.unsqueeze(1), payout_mat_2), y.unsqueeze(-1))
+#         return [L_1.squeeze(-1), L_2.squeeze(-1), None]
+#     return dims, Ls
+
+def ipd_one_iteration_batched(bs, gamma_inner=0.96):
+    dims = [1, 1]
+    payout_mat_1 = torch.Tensor([[-1, -3], [0, -2]]).to(device)
+    payout_mat_2 = payout_mat_1.T
+    payout_mat_1 = payout_mat_1.reshape((1, 2, 2)).repeat(bs, 1, 1).to(device)
+    payout_mat_2 = payout_mat_2.reshape((1, 2, 2)).repeat(bs, 1, 1).to(device)
+
+    def Ls(th):
+        p_1, p_2 = torch.sigmoid(th[0]), torch.sigmoid(th[1])
+        x, y = torch.cat([p_1, 1-p_1], dim=-1), torch.cat([p_2, 1-p_2], dim=-1)
+        L_1 = -torch.matmul(torch.matmul(x, payout_mat_1), y.unsqueeze(-1))
+        L_1 = torch.reshape(L_1, (bs,1,1)) 
+        L_2 = -torch.matmul(torch.matmul(x, payout_mat_2), y.unsqueeze(-1))
+        L_2 = torch.reshape(L_2, (bs,1,1))
+        return [L_1.squeeze(-1), L_2.squeeze(-1), None]
+    return dims, Ls
+
+def get_gradient(function, param):
+    grad = torch.autograd.grad(function, param, create_graph=True, allow_unused=True)[0]
+    return grad
+
+
+def compute_best_response(outer_th_ba):
+    batch_size = 1
+    std = 0
+    num_steps = 1000
+    lr = 1
+
+    ipd_batched_env = ipd_batched(batch_size, gamma_inner=0.96)[1]
+    inner_th_ba = torch.nn.init.normal_(torch.empty((batch_size, 5), requires_grad=True), std=std).cuda()
+    for i in range(num_steps):
+        th_ba = [inner_th_ba, outer_th_ba.detach()]
+        l1, l2, M = ipd_batched_env(th_ba)
+        grad = get_gradient(l1.sum(), inner_th_ba)
+        with torch.no_grad():
+            inner_th_ba -= grad * lr
+    print(l1.mean() * (1 - 0.96))
+    return inner_th_ba
+
+
+def generate_mamaml(b, d, inner_env, game, inner_lr=1):
+    """
+    This is an improved version of the algorithm presented in this paper:
+    https://arxiv.org/pdf/2011.00382.pdf
+    Rather than calculating the loss using multiple policy gradients terms,
+    this approach instead directly takes all of the gradients through because the environment is differentiable.
+    """
+    outer_lr = 0.01
+    mamaml = torch.nn.init.normal_(torch.empty((1, d), requires_grad=True, device=device), std=1.0)
+    alpha = torch.rand(1, requires_grad=True, device=device)
+
+    optimizer = torch.optim.Adam([mamaml, alpha], lr=outer_lr)
+
+    for ep in range(1000):
+        agent = mamaml.clone().repeat(b, 1)
+        opp = torch.nn.init.normal_(torch.empty((b, d), requires_grad=True), std=1.0).cuda()
+        total_agent_loss = 0
+        total_opp_loss = 0
+        for step in range(100):
+            l1, l2, M = inner_env([opp, agent])
+            total_agent_loss = total_agent_loss + l2.sum()
+            total_opp_loss = total_opp_loss + l1.sum()
+
+            opp_grad = get_gradient(l1.sum(), opp)
+            agent_grad = get_gradient(l2.sum(), agent)
+            opp = opp - opp_grad * inner_lr
+            agent = agent - agent_grad * alpha
+
+        optimizer.zero_grad()
+        total_agent_loss.sum().backward()
+        optimizer.step()
+        print(total_agent_loss.sum().item())
+
+    torch.save((mamaml, alpha), f"mamaml_{game}.th")
+
+
+class MetaGames:
+    def __init__(self, b, opponent="NL", game="IPD", mmapg_id=0):
+        """
+        Opponent can be:
+        NL = Naive Learner (gradient updates through environment).
+        LOLA = Gradient through NL.
+        STATIC = Doesn't learn.
+        COPYCAT = Copies what opponent played last step.
+        """
+        self.gamma_inner = 0.96
+        self.b = b
+        self.num_actions = 2
+
+        d, self.game_batched = ipd_one_iteration_batched(b, gamma_inner=self.gamma_inner)
+        self.action_space = gym.spaces.Tuple([gym.spaces.Discrete(self.num_actions), gym.spaces.Discrete(self.num_actions)])
+        self.std = 1
+        self.lr = 1
+        self.d = d[0]
+
+        self.opponent = opponent
+        if self.opponent == "MAMAML":
+            f = f"data/mamaml_{self.game}_{mmapg_id}.th"
+            assert osp.exists(f), "Generate the MAMAML weights first"
+            self.init_th_ba = torch.load(f)
+        else:
+            self.init_th_ba = None
+            
+    def reset(self, info=False):
+        if self.init_th_ba is not None:
+            self.inner_th_ba = self.init_th_ba.detach() * torch.ones((self.d), requires_grad=True).to(device)
+        else:
+            self.inner_th_ba = torch.nn.init.normal_(torch.empty((self.d), requires_grad=True), std=self.std).to(device)
+        outer_th_ba = torch.nn.init.normal_(torch.empty((self.d), requires_grad=True), std=self.std).to(device)
+        state, _, _, M = self.step(outer_th_ba)
+        if info:
+            return state, M
+        else:
+            return state
+
+    def step(self, outer_th_ba):
+        last_inner_th_ba = self.inner_th_ba.detach().clone()
+        th_ba = [self.inner_th_ba, outer_th_ba.detach()]
+        l1, l2, M = self.game_batched(th_ba)
+        grad = get_gradient(l1.sum(), self.inner_th_ba)
+        with torch.no_grad():
+            self.inner_th_ba -= grad * self.lr
+
+        return torch.sigmoid(torch.cat((outer_th_ba, last_inner_th_ba), dim=-1)).detach(), (-l2 * (1 - self.gamma_inner)).detach(), (-l1 * (1 - self.gamma_inner)).detach(), M
+       
+    
+    
+    
---
+++ Work/rmax.py
@@ -0,0 +1,100 @@
+#Rmax code, cuda not yet incompatible
+import math
+import numpy as np
+import torch
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Memory:
+    def __init__(self):
+        self.actions = []
+        self.states = []
+        self.logprobs = []
+        self.rewards = []
+
+    def clear_memory(self):
+        del self.actions[:]
+        del self.states[:]
+        del self.logprob[:]
+        del self.rewards[:]
+        
+class RmaxAgent:
+
+    def __init__(self, env, R_max, gamma, max_episodes, max_steps, radius_dp, epsilon = 0.2):
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.radius_dp = radius_dp               #added decimal place for Q & R matrix dimension
+        self.max_steps = max_steps
+        self.max_episodes = max_episodes
+        self.b = env.b
+        #Q for meta-game, *2 for 2 player
+        #self.Q = torch.ones(((10**(radius_dp)+1) * 2, env.num_actions)).mul(R_max / (1 - self.gamma)).to(device)   
+        self.Q = torch.ones(self.b, ((10**(radius_dp)+1) * env.num_actions * 2), ((10**(radius_dp)+1) * env.num_actions)).mul(R_max / (1 - self.gamma)).to(device) 
+        #self.R = torch.zeros(((10**(radius_dp)+1) * 2, env.num_actions)).to(device)  
+        self.R = torch.zeros(self.b, ((10**(radius_dp)+1) * env.num_actions * 2), ((10**(radius_dp)+1) * env.num_actions)).to(device)
+        
+        self.nSA = torch.zeros(self.b, ((10**(radius_dp)+1) * env.num_actions * 2), ((10**(radius_dp)+1) * env.num_actions)).to(device)
+        
+        self.nSAS = torch.ones(self.b, ((10**(radius_dp)+1) * env.num_actions * 2), ((10**(radius_dp)+1) * env.num_actions), ((10**(radius_dp)+1) * env.num_actions * 2)).to(device)
+        
+        self.val1 = []
+        self.val2 = []  #This is for keeping track of rewards over time and for plotting purposes  
+        self.m = int(math.ceil(math.log(1 / (self.epsilon * (1-self.gamma))) / (1-self.gamma)))   #calculate m number
+        
+    def select_action(self, state):
+        if np.random.random() > (1-self.epsilon):
+            action = env.action_space.sample()
+        else:
+            action = torch.amax(self.Q[:,state,:], 2)
+        return action     #returns action of length b
+    
+    def update(self, memory, best_action, state):
+        for i in range(self.b):
+            if self.nSA[i][state[i]][best_action] < self.m:
+                self.nSA[i][state[i]][best_action] +=1
+                self.R[i][state[i]][best_action] += reward
+                self.nSAS[i][state[i]][best_action][new_obs] += 1
+
+                if self.nSA[i][state[i]][best_action] == self.m:
+
+                    for i in range(mnumber):
+
+                        for s in range(env.d * 2):
+
+                            for a in range(env.d):
+
+                                if self.nSA[i][s][a] >= self.m:
+
+                                    #In the cited paper it is given that reward[s,a]= summation of rewards / nSA[s,a]
+                                    #We have already calculated the summation of rewards in line 28
+                                    q = (self.R[i][s][a]/self.nSA[i][s][a])
+
+                                    for next_state in range(env.d * 2):
+
+                                        #In the cited paper it is given that transition[s,a] = nSAS'[s,a,s']/nSA[s,a]
+
+                                        transition = self.nSAS[i][s][a][next_state]/self.nSA[i][s][a]
+                                        q += (transition * torch.max(self.Q[i,next_state,:]))
+
+                                    self.Q[i][s][a] = q 
+                                    #print(q + self.gamma*(self.R[state][action]/self.nSA[state][action]))
+                                    #In the cited paper it is given that reward[s,a]= summation of rewards / nSA[s,a]
+                                    #We have already calculated the summation of rewards in line 28
+
+            memory.state = new_state  
+
+    def mean_rewards_per_500(self):
+        
+        total_reward = 0
+        for episodes in range(500):
+            state = env.reset()
+            for _ in range(1000):
+
+                action = self.choose_action(state)
+                state, reward, done, info = env.step(action)
+                total_reward += reward
+                if done:
+                    state = env.reset()
+                    break
+        return (total_reward/500) 
+
+    
