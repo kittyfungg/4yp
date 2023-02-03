0da76ae7fcdf0c7a4497b86ff7128330c47d2453
Kitty Fung <magd5245@htc-login01.arcus-htc.arc.local>
2023-02-03 19:31:15 +0000

    unbatched, inner-step&epi all gone, synchronised

--- Work/env_mp.py
+++ Work/env_mp.py
@@ -1,48 +1,32 @@
 import torch
+import numpy as np
 import os.path as osp
 import gym
+from gym.spaces import Discrete, Tuple
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-       
-# def pd_one_iteration_batched(bs):
-#     dims = [1, 2]
-#     #/5 to cap reward to 1 for rmax, [0,0] CC, [1,1] DD
-#     payout_mat_1 = torch.Tensor([[3/5, 0], [5/5, 1/5]]).to(device)
-#     payout_mat_2 = payout_mat_1.T
-#     rew1 = torch.empty(bs).to(device)
-#     rew2 = torch.empty(bs).to(device)
 
-#     def Reward(action):
-#         for i in range(bs):
-#             x = torch.stack((action[0,i], 1-action[0,i]), dim=0)    #[our agent, updated action, batch]
-#             y = torch.stack((action[1,i], 1-action[1,i]), dim=0)
-#             rew1[i] = torch.matmul(torch.matmul(x, payout_mat_1), y.unsqueeze(-1)).squeeze(-1)
-#             rew2[i] = torch.matmul(torch.matmul(x, payout_mat_2), y.unsqueeze(-1)).squeeze(-1)
-#         return [rew1, rew2]
 
-#     return dims, Reward
-
-def mp_one_iteration_batched(bs):
+def mp_one_iteration():
     dims = [1, 2]
     #/5 to cap reward to 1 for rmax
-    payout_mat_1 = torch.Tensor([[0, 1], [1, 0]]).to(device)    #changed -1 entries to 0 since cant negative-index
-    payout_mat_2 = payout_mat_1.T
-    rew1 = torch.empty(bs).to(device)
-    rew2 = torch.empty(bs).to(device)
+    payout_mat_1 = torch.Tensor([[1, 0], [0, 1]]).to(device)    #changed -1 entries to 0 since cant negative-index
+    payout_mat_2 = torch.Tensor([[0, 1], [1, 0]]).to(device)
+    rew1 = 0
+    rew2 = 0
     
     def Reward(action):
-        for i in range(bs):
-            x = torch.stack((action[0,i], 1-action[0,i]), dim=0)    #[our agent, updated action, batch]
-            y = torch.stack((action[1,i], 1-action[1,i]), dim=0)
-            rew1[i] = torch.matmul(torch.matmul(x, payout_mat_1), y.unsqueeze(-1)).squeeze(-1).detach().clone()
-            rew2[i] = torch.matmul(torch.matmul(x, payout_mat_2), y.unsqueeze(-1)).squeeze(-1).detach().clone()
+        x = torch.stack((action[0], 1-action[0]), dim=0)    #[our agent, updated action, batch]
+        y = torch.stack((action[1], 1-action[1]), dim=0)
+        rew1= torch.matmul(torch.matmul(x, payout_mat_1), y.unsqueeze(-1)).squeeze(-1).detach().clone()
+        rew2= torch.matmul(torch.matmul(x, payout_mat_2), y.unsqueeze(-1)).squeeze(-1).detach().clone()
 
         return [rew1, rew2]
     
     return dims, Reward
 
 class MetaGames:
-    def __init__(self, b, opponent="NL", game="IPD"):
+    def __init__(self, opponent="NL", game="IPD"):
         """
         Opponent can be:
         NL = Naive Learner (gradient updates through environment).
@@ -51,41 +35,47 @@ class MetaGames:
         COPYCAT = Copies what opponent played last step.
         """
         self.gamma_inner = 0.96
-        self.b = b
         
-        d, self.game_batched = mp_one_iteration_batched(b)
-        self.std = 1
+        d, self.game= mp_one_iteration()
+        self.epsilon = 0.8
         self.lr = 1
         self.d = d[0]    
         self.num_actions = d[1]
         self.num_agents = 2
+        self.action_space = Tuple([Discrete(self.num_actions), Discrete(self.num_actions)])
         
-        self.rew1 = torch.empty(b).to(device)
-        self.rew2 = torch.empty(b).to(device)
+        self.rew1 = 0
+        self.rew2 = 0
         
-        #reward table with discretized dimensions, (batch_size, actions, agents)
-        self.innerr = torch.zeros(self.b, self.num_actions, self.num_agents).to(device) 
-        self.innerq = torch.zeros(self.b, self.num_actions, self.num_agents).to(device)     
+        #reward table with discretized dimensions, (actions, agents) (no states since num of state =1)
+        self.innerr = torch.zeros(self.num_agents, self.num_actions).to(device) 
+        self.innerq = torch.zeros(self.num_agents, self.num_actions).to(device)     
 
     def reset(self, info=False):
-        #random action of size [2 agents, size.b], action value either 0 (Coorperate) or 1 (Defect)
-        self.init_action = torch.randint(2, (self.num_agents, self.b)).to(device)
-        self.innerr = torch.zeros(self.b, self.num_actions, self.num_agents).to(device) 
-        #return state
+        #random action of size [2 agents], action value either 0 (Coorperate) or 1 (Defect)
+        self.init_action = torch.randint(2, (self.num_agents, )).to(device)
+        #Initialise inner R table as 0
+        self.innerr = torch.zeros(self.num_agents, self.num_actions).to(device) 
 
+    def select_action(self):
+        if np.random.random() > (1-self.epsilon):
+            action = torch.Tensor(list(self.action_space.sample())).to(device)   #convert tuple-->tensor
+        else:
+            action = torch.argmax(self.innerq, 1).to(device)    #find maximum from the second dimension(action dimension)
+        return action     #returns action for all agents
+        
     def step(self, action):
-        r1, r2 = self.game_batched(action.float())
+        r1, r2 = self.game(action.float())
         self.rew1 = r1.detach().clone()
         self.rew2 = r2.detach().clone()
         return r1.detach(), r2.detach()
     
     def choose_action(self):
     #chooses action that corresponds to the max Q value of the particular agent
-        best_action = torch.empty((self.num_agents, self.b), dtype = torch.int64).to(device)
+        best_action = torch.empty((self.num_agents), dtype = torch.int64).to(device)
         
-        for i in range(self.b):
-            best_action[0,i] = torch.argmax(self.innerq[i, :, 0])
-            best_action[1,i] = torch.argmax(self.innerq[i, :, 1])
+        best_action[0] = torch.argmax(self.innerq[0, :])
+        best_action[1] = torch.argmax(self.innerq[1, :])
         return best_action.detach()
             
        
--- Work/rmax_1.py
+++ Work/rmax_1.py
@@ -4,14 +4,6 @@ import numpy as np
 import torch
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def find_meta_index(meta, radius, poss_combo):
-    index = int(0) #initialise index
-
-    #for every digit in metastate/ metaaction:
-    for i in range(list(meta.size())[0]):
-        index += (meta[i]//radius) * (poss_combo ** i)
-        
-    return int(index.item())
 
 class Memory:
     def __init__(self):
@@ -30,61 +22,83 @@ class RmaxAgent:
         self.inner_gamma = inner_gamma
         self.epsilon = epsilon
         self.radius = radius               #added decimal place for Q & R matrix dimension
-        self.b = env.b
+        #self.action_space = []
        
-        #no of possible combinations for a inner Q value
+        #no of possible combinations for an inner Q value
         self.poss_combo = math.ceil((1//(1-inner_gamma)) / radius) +1
         self.meta_size = self.poss_combo ** (env.d * env.num_actions)
         
-        #Q = [batch, meta_state, meta_action, *2 for 2 player  
-        self.Q = torch.ones(self.b, self.meta_size * 2, self.meta_size).mul(R_max / (1 - self.meta_gamma)).to(device) 
-        self.R = torch.ones(self.b, self.meta_size * 2, self.meta_size).to(device)
-        self.nSA = torch.zeros(self.b, self.meta_size * 2, self.meta_size).to(device)
-        self.nSAS = torch.ones(self.b, self.meta_size * 2, self.meta_size, self.meta_size * 2).to(device)
+        #Q = [meta_state, meta_action], **2 for 2 player  
+        self.Q = torch.ones(self.meta_size ** 2, self.meta_size).mul(R_max / (1 - self.meta_gamma)).to(device) 
+        self.R = torch.ones(self.meta_size ** 2, self.meta_size).to(device)
+        self.nSA = torch.zeros(self.meta_size ** 2, self.meta_size).to(device)
+        self.nSAS = torch.ones(self.meta_size ** 2, self.meta_size, self.meta_size ** 2).to(device)
     
         self.val1 = []
         self.val2 = []  #This is for keeping track of rewards over time and for plotting purposes  
         self.m = int(math.ceil(math.log(1 / (self.epsilon * (1-self.meta_gamma))) / (1-self.meta_gamma)))   #calculate m number
         
     def select_action(self, env, state):
-        #if np.random.random() > (1-self.epsilon):
-        #    action = env.action_space.sample()
-        #else:
-        action = torch.argmax(self.Q[:,find_meta_index(torch.flatten(state), self.radius, self.poss_combo),:], 1)    #find maximum from the third dimension(action dimension)
-        return action     #returns action of length b
+        if np.random.random() > (1-self.epsilon):
+            action = torch.randint(self.meta_size)
+        else:
+            #find maximum action index, given state
+            action = torch.argmax(self.Q[find_meta_index(torch.flatten(state), self.radius, self.poss_combo),:])    
+        return action     #returns action index
     
+    def find_meta_index(self, meta):
+        index = int(0) #initialise index
+
+        #for every digit in metastate/ metaaction:
+        for i in range(list(meta.size())[0]):
+            index += (meta[i]//self.radius) * (self.poss_combo ** i)
+        return int(index)
+    
+    def index_to_table(self, env, index, agent_size) :
+        #returns a table of size [agent, num_actions], given index
+        maxi = agent_size * env.num_actions 
+        reconstruct = torch.empty(maxi)
+        for i in reversed(range(maxi)):
+            if index >= self.poss_combo**i:
+                q, mod = divmod(index, self.poss_combo**i)
+                reconstruct[i] = q * self.radius     #recover original discretized value
+                index = mod
+            else:
+                q = 0
+                mod = 0
+                reconstruct[i] = 0
+        
+        return torch.reshape(reconstruct, (agent_size, env.num_actions))
+
     def update(self, env, memory, state, action, next_state):
-        #for each batch
-        for batch in range(self.b):
-            action_mapped = find_meta_index( torch.flatten( action[batch] ), self.radius, self.poss_combo)
-            state_mapped = find_meta_index( torch.flatten( state[batch] ), self.radius, self.poss_combo)
-            next_state_mapped = find_meta_index( torch.flatten( next_state[batch] ), self.radius, self.poss_combo)
+        action_mapped = self.find_meta_index( torch.flatten(action))
+        state_mapped = self.find_meta_index( torch.flatten(state))
+        next_state_mapped = self.find_meta_index( torch.flatten(next_state))
 
-            if self.nSA[batch][state_mapped][action_mapped] < self.m:
-                
-                self.nSA[batch][state_mapped][action_mapped] += 1
-                self.R[batch][state_mapped][action_mapped] += memory.rewards[-1][batch]
-                self.nSAS[batch][state_mapped][action_mapped][next_state_mapped] += 1
+        if self.nSA[state_mapped][action_mapped] < self.m:
 
-                if self.nSA[batch][state_mapped][action_mapped] == self.m:
+            self.nSA[state_mapped][action_mapped] += 1
+            self.R[state_mapped][action_mapped] += memory.rewards[-1]
+            self.nSAS[state_mapped][action_mapped][next_state_mapped] += 1
 
-                    for i in range(self.m):
+            if self.nSA[state_mapped][action_mapped] == self.m:
 
-                        for s in range(self.meta_size * 2):
-                        
-                            for a in range(self.meta_size):
+                for i in range(self.m):
 
-                                if self.nSA[batch][s][a] >= self.m:
+                    for s in range(self.meta_size * 2):
 
-                                    #We have already calculated the summation of rewards in line 28
-                                    q = (self.R[batch][s][a]/self.nSA[batch][s][a])
+                        for a in range(self.meta_size):
 
-                                    for next_s in range(env.d * 2):
-                                        transition = self.nSAS[batch][s][a][next_s]/self.nSA[batch][s][a]
-                                        q += (transition * torch.max(self.Q[batch,next_s,:]))
+                            if self.nSA[s][a] >= self.m:
 
-                                    self.Q[batch][s][a] = q 
-                                    #We have already calculated the summation of rewards in line 28
+                                #We have already calculated the summation of rewards in line 28
+                                q = (self.R[s][a]/self.nSA[s][a])
+
+                                for next_s in range(env.d * 2):
+                                    transition = self.nSAS[s][a][next_s]/self.nSA[s][a]
+                                    q += (transition * torch.max(self.Q[next_s,:]))
+
+                                self.Q[s][a] = q 
+                                #We have already calculated the summation of rewards in line 28
 \
 
-        
