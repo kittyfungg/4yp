a4b79a657d43f700227c8e6b8a84a1cfec9bb063
Chris Lu <luchris429@gmail.com>
2023-03-06 11:39:52 +0000

    Even simpler

--- env_mp_simple.py
+++ env_mp_simple.py
@@ -107,4 +107,40 @@ class MetaGamesSimple:
 
         return observation, r1, done, {"r1": r1, "r2": r2}
     
+class MetaGamesSimplest:
+    def __init__(self):
+        self.epsilon = 0.8
+        self.lr = 1.0
+        #reward table with discretized dimensions, (actions, agents) (no states since num of state =1)
+
+    def reset(self):
+        #Initialise inner policy randomly
+        self.inner_policy = np.random.randint(0, 2)
+        self.init_action = np.random.randint(0,2)
+        self.t = 0
+        return np.concatenate([self.inner_policy, self.init_action, self.t], axis=0) # OBS: INNER_ACT, ACTION, TIMESTEP
+
+    def step(self, action):
+        opponent_action = self.inner_policy
+
+        # PLAY GAME, GET REWARDS
+        r1 = int(opponent_action == action)
+        r2 = 1 - r1
+        self.t += 1
+
+        # GENERATE OBSERVATION
+        observation = np.concatenate([
+            self.inner_policy,
+            action,
+            self.t
+        ], axis=0) # MAKE SURE SHAPES LINE UP
+
+        # UPDATE OPPONENT POLICY
+        self.inner_policy = 1 - action
+
+        # CHECK DONE
+        done = self.t > 10
+
+        return observation, r1, done, {"r1": r1, "r2": r2}
+    
        
